b0ce5a0deb27c1dcc95a6b05983fbf7dfd607bf9
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-12 20:15:09 +0300

    AS

--- src/main/java/org/example/Healthplan.java
+++ src/main/java/org/example/Healthplan.java
@@ -1,19 +1,20 @@
 package org.example;
 
 import org.example.enums.Plan;
-package org.example;
 
-import org.example.enums.Plan;
 
 public class Healthplan {
+
     private int id;
     private String name;
     private Plan plan;
+
 
     public Healthplan(int id, String name, Plan plan) {
         this.id = id;
         this.name = name;
         this.plan = plan;
+
     }
 
     public int getId() {
@@ -28,7 +29,7 @@ public class Healthplan {
         return name;
     }
 
-    public void setName(String name) {
+    public void setId(String name) {
         this.name = name;
     }
 
@@ -40,44 +41,7 @@ public class Healthplan {
         this.plan = plan;
     }
 
-    @Override
     public String toString() {
         return "Healthplan{id=" + id + ", name='" + name + "', plan=" + plan.getName() + ", price=" + plan.getPrice() + "}";
-    }
-}
-
-public class Healthplan {
-
-    private int id;
-    private String name;
-    private Plan plan;
-    public Healthplan(int id, String name,Plan plan){
-        this.id=id;
-        this.name=name;
-        this.plan=plan;
-
-    }
-    public int getId(){
-        return id;
-    }
-    public  void  setId(int id){
-        this.id=id;
-    }
-    public String getName(){
-        return name;
-    }
-    public  void  setId(String name){
-        this.name=name;
-    }
-
-    public Plan getPlan() {
-        return plan;
-    }
-    public void setPlan(Plan plan) {
-        this.plan=plan;
-    }
-
-    public String toString(){
-        return  "Healthplan{id=" + id + ", name='" + name + "', plan=" + plan.getName() + ", price=" + plan.getPrice() + "}";
     }
 }
